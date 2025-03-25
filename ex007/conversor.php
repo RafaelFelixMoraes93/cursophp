faf900501422be8147377c64ee18ba166a87dedb
Rafael <rafael.felix.moraes@gmail.com>
2025-03-25 17:23:54 -0300

    "Feat desafio conversor de moedas fixo"

---
+++ ex007/conversor.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de Moraes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+    <?php 
+        $valor = $_GET["valor"] ?? "Valor inválido";
+        echo "<h1><strong>Conversor de Moedas v1.0</strong></h1>" . "<br>";
+        echo "Seus R$ " . $valor . " equivalem a <strong>" . $valor * 5.765 . "</strong>." ."<br>";
+        echo "<strong>Cotação fixa de 5.765</strong> informada diretamente no código."
+    ?>
+    <a href="http://localhost/cursophp/ex007/"><button type="button">Voltar</button></a>  
+    </section>
+</body>
+</html>
