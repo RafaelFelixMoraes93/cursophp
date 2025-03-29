f304e36237dddecfa5b3320f65df4fa623b757ff
Rafael <rafael.felix.moraes@gmail.com>
2025-03-29 20:55:09 -0300

    "Feat Adiciando FOR"

---
+++ 008 - for/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For</title>
+</head>
+<body>
+    <main>
+        <?php 
+            for($x = 0; $x <= 10; $x++){
+                echo "Contando " . $x . "<br>";
+            }
+        ?>
+    </main>    
+</body>
+</html>
