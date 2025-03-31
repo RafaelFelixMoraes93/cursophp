9f13d59fc408922195bb9a872835309263579aa2
Rafael <rafael.felix.moraes@gmail.com>
2025-03-31 18:22:50 -0300

    "Feat Adicinando função print_r"

---
+++ 012 - funcoes string/funcao print_r/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pr_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demonstração PRINT_R</title>
+</head>
+<body>
+    <?php
+        $x[0] = 4;
+        $x[1] = 3;
+        $x[2] = 8;
+
+        echo "<h1>Imprimindo o vetor com print_r</h1>";
+        print_r($x);
+
+        $x2 = array (3, 7, 6, 2, 1, 9);
+        echo "<h1>Segundo exemplo de vetor com print_r</h1>";
+        print_r($x2);
+    ?>
+    
+</body>
+</html>
