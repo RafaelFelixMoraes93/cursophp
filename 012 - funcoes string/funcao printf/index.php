c22ad5ddeb3e3cd33d038b6082225bae40740168
Rafael <rafael.felix.moraes@gmail.com>
2025-03-31 18:15:10 -0300

    "Refactor Adicionado comentários"

--- 012 - funcoes string/funcao printf/index.php
+++ 012 - funcoes string/funcao printf/index.php
@@ -7,7 +7,13 @@
 </head>
 <body>
     <main>
-        <?php 
+        <?php
+            /*
+            * %d valor decimal (positivo ou negativo)
+            * %u valor decial sem sinal (apenas positivos)
+            * %f valor real (devemos colocar .N para as casas * decimais %.02f / %0.1f)
+            * %s string
+            */
             $produto = "Leite";
             $preco = 4.50;
             echo "<h1>Demonstração PRINTF</h1>";
