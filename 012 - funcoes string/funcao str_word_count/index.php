7e59d5a690d8a86b5d254daef9547bbf0ea88ac2
Rafael <rafael.felix.moraes@gmail.com>
2025-03-31 19:18:51 -0300

    "Refactor Adicionando mais conhecimento ao código"

--- 012 - funcoes string/funcao str_word_count/index.php
+++ 012 - funcoes string/funcao str_word_count/index.php
@@ -9,10 +9,18 @@
     <main>
         <?php 
             $frase = "Eu vou estudar PHP";
+            $contando = str_word_count($frase, 1);
+            $contando2 = str_word_count($frase, 2);
 
             echo "<h1>Conta quantas palavras tem em uma frase.</h1>";
             
             printf("Frase: %s, quantidade de palavras: %d.", $frase, str_word_count($frase));
+            
+            echo "<strong><p>Podemos verificar em forma de array quantos paragrafos existem<p></strong>";
+            print_r($contando);
+
+            echo "<p><strong>Ainda é possível marcar a posição do inicio de cada palavra.<p></strong>";
+            print_r($contando2);
         ?>
     </main>    
 </body>
