8cfaebaa3debf5ef4823cbbef9c05943ce03d164
Rafael <rafael.felix.moraes@gmail.com>
2025-04-02 20:43:13 -0300

    "Feat Adicionado String to Lower"

---
+++ 012 - funcoes string/funcao strtolower/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String to Lower</title>
+</head>
+<body>
+    <?php 
+        $nome = "Rafael Felix";
+        printf(strtolower($nome));
+    ?>    
+</body>
+</html>
