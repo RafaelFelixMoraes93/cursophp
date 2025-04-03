023cf75a0985c523524e2052da9052b413feea74
Rafael <rafael.felix.moraes@gmail.com>
2025-04-02 21:00:06 -0300

    "Feat Adicionado funcao strpos"

---
+++ 012 - funcoes string/funcao strpos/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $frase = "Estou aprendendo PHP";
+        $pos = strpos($frase, "PHP");
+        echo "$frase <br> A string foi encontrada na posição $pos";
+    ?>
+    
+</body>
+</html>
